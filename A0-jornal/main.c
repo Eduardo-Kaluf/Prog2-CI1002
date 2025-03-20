e53106b4b8c3d9f4a2b8d8ed061cb8ca9f6781b6
eduardo.kaluf <eduardo.kaluf@iotag.com.br>
2025-03-20 13:22:30 -0300

    implementando a main dos jornais

--- A0-jornal/main.c
+++ A0-jornal/main.c
@@ -8,7 +8,7 @@ struct node_t {
     char titulo[33];
     char texto[513];
 
-    char idade; // !NUMERO!
+    char idade;
 };
 
 struct fila_t {
@@ -63,7 +63,7 @@ void insere_noticia(struct fila_t *f, struct node_t *noticia) {
         return;
     }
 
-    // Caso a fila não esteja vazia
+    // Caso a fila nao esteja vazia
     f->tail->prox = noticia;
     noticia->prox = NULL;
     f->tail = noticia;
@@ -85,7 +85,7 @@ void atualizar (struct fila_t *f) {
     struct node_t *p = f->head;
 
     if (f == NULL)
-        return NULL;
+        return;
     
     int i = 0;
     while (p != NULL) {
@@ -105,7 +105,7 @@ void atualizar (struct fila_t *f) {
 
 void requisita_noticia(char *titulo, char *texto){
     getchar();
-    printf("\nDigite o título: ");
+    printf("\nDigite o titulo: ");
     fgets(titulo, 33, stdin);
     printf("Digite o texto: ");
     fgets(texto, 513, stdin);
@@ -125,13 +125,124 @@ struct node_t *criar_noticia() {
 }
 
 void print_menu() {
-    printf(" - (1) Cadastrar notícia");
-    printf(" - (2) Fechar edição");
+    printf(" - (1) Cadastrar noticia");
+    printf(" - (2) Fechar edicao");
     printf(" - (3) Sair");
 }
 
-int main() {
+int tam_fila(struct fila_t *f) {
+    struct node_t *aux;
+    int tam = 0;
 
+    if (f == NULL) {
+        return tam;
+    }
+
+    aux = f->head;
+
+    while (aux != NULL) {
+        tam++;
+
+        aux = aux->prox;
+    }
+
+    return tam;
+}
+
+void print_noticia(struct node_t *noticia) {    
+    char c = 'e';
+    int i = 0;
+    while (c != '\0') {
+        c = noticia->titulo[i];
+
+        printf("%c", c);
+        
+        i++;
+    }
+    
+    printf("\n\n");
+
+    i = 0;
+    c = 'e';
+
+    while (c != '\0') {
+        c = noticia->texto[i];
+
+        printf("%c", c);
+        
+        i++;
+    }
+
+    printf("\n\n");
+
+}
+
+int main() {
+    int option = 0;
+    int tipo_noticia;
+    struct fila_t* breaking_fila = cria_fila();
+    struct fila_t* informe_fila = cria_fila();
+    struct node_t* noticia1;
+    struct node_t* noticia2;
+    
+
+    while (option != 3) {
+        print_menu();
+        scanf("%d", &option);
+
+        if (option == 1) {
+            scanf("%d", &tipo_noticia);
+            if (!tipo_noticia)
+                insere_noticia(breaking_fila, criar_noticia());
+            else
+                insere_noticia(informe_fila, criar_noticia());
+        }
+
+        if (option == 2) {
+            int tam_b = tam_fila(breaking_fila);
+            int tam_i = tam_fila(informe_fila);
+
+            if (tam_b >= 2) {
+                noticia1 = remove_noticia(breaking_fila);
+                noticia2 = remove_noticia(breaking_fila);
+            }
+            else if (tam_b == 1 && tam_fila(informe_fila) >= 1) {
+                noticia1 = remove_noticia(breaking_fila);
+                noticia2 = remove_noticia(informe_fila);
+            }   
+            else if (tam_b == 1 && tam_i == 0) {
+                noticia1 = remove_noticia(breaking_fila);
+                noticia2 = NULL;
+            }  
+            else if (tam_i >= 2) {
+                noticia1 = remove_noticia(informe_fila);
+                noticia2 = remove_noticia(informe_fila);
+
+            } 
+            else if (tam_i == 1) {
+                noticia1 = remove_noticia(informe_fila);
+                noticia2 = NULL;
+            }
+            else {
+                noticia1 = NULL;
+                noticia2 = NULL;
+            }
+
+            if (noticia1) {
+                printf("=======================================================\n");
+                print_noticia(noticia1);
+            }
+            if (noticia2) {
+                printf("==");
+                print_noticia(noticia2);
+                printf("=======================================================\n");
+            }
+
+            atualizar(breaking_fila);
+            atualizar(informe_fila);
+        }
+
+    }
 
     return 0;
 }
